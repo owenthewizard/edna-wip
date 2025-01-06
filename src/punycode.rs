148ec2a01223b3421eeebb8c0b89ac799a4c19ab
owenthewizard <owen@walpole.dev>
2025-01-05 20:58:26 -0600

    remove inaccurate comment

--- src/punycode.rs
+++ src/punycode.rs
@@ -33,9 +33,7 @@ type Utf32 = Vec<char>;
 #[must_use]
 #[expect(clippy::cast_possible_truncation)]
 pub fn encode(input: &str) -> String {
-    // UTF-32 is 4x as many bytes as UTF-8 in the worst case, but may be less
     let mut new_input = Utf32::with_capacity(input.len() * 4);
-    // ASCII-Unicode split is unknown, same worst case as above
     let mut output = String::with_capacity(input.len() * 4);
     let mut non_ascii = Utf32::with_capacity(input.len() * 4);
 
